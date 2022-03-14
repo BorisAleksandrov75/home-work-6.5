b063c3446e277cb7219d004d0b439eede122343d
Boris Aleksandrov <aleksandrov.bg78@gmail.com>
2022-03-14 20:47:34 +0900

    Добавил класс квадрат, выполнил первый тест - работает.

--- src/Figure/simpleFigure.java
+++ src/Figure/simpleFigure.java
@@ -1,4 +1,25 @@
 package Figure;
 
 public class simpleFigure {
+    protected int a , b;
+
+    public simpleFigure() {
+    }
+
+    public simpleFigure(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int areaCalculation() {
+        int c = a * b;
+        return c;
+    }
+
+    public int perimetrCalculation() {
+        int c = (a * 2) + (b * 2);
+        return c;
+    }
 }
+
+
--- src/main.java
+++ src/main.java
@@ -1,2 +1,11 @@
+import Figure.square;
+
 public class main {
+
+    public static void main(String[] args) {
+        square square1 = new square(4,4);
+        System.out.println(square1.areaCalculation());
+        System.out.println(square1.perimetrCalculation());
+    }
+
 }
